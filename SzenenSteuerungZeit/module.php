1816c17faefea9fe24f7a1f69eff98a41d5b494f
pro-internet <support@prointernet.de>
2017-04-10 20:01:04 +0200

    SceneCount auf 2 gesetzt

--- SzenenSteuerungZeit/module.php
+++ SzenenSteuerungZeit/module.php
@@ -1,13 +1,13 @@
 <?
 class SzenenSteuerungZeit extends IPSModule {
-# Test von Sven
+
 	public function Create() {
 		//Never delete this line!
 		parent::Create();
 
 		//These lines are parsed on Symcon Startup or Instance creation
 		//You cannot use variables here. Just static values.
-		$this->RegisterPropertyInteger("SceneCount", 3);
+		$this->RegisterPropertyInteger("SceneCount", 2);
 		$this->RegisterPropertyBoolean("CycleThrough", true);
 		$this->RegisterPropertyBoolean("Loop", false);
 		
