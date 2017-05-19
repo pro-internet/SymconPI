f4b8b7afd9dbb25815465d73a9aa6c62ff8b1ddf
Alex Wellnitz <wellnitz.alex@gmail.com>
2017-05-19 11:43:09 +0200

    bug fix

--- DMXDyn/module.php
+++ DMXDyn/module.php
@@ -16,7 +16,7 @@
             // Create Instance Vars
             $VarID_RWert = IPS_CreateVariable(1);
             IPS_SetName($VarID_RWert, "R Standart Wert");
-            IPS_SetParent($VarID_RWert, 44440);
+            IPS_SetParent($VarID_RWert, 30400 );
 
           
           /*
