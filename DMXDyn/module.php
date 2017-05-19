6156b23c393098ea112414d0816f663d7888be14
Alex Wellnitz <wellnitz.alex@gmail.com>
2017-05-19 11:34:39 +0200

    add CreateVariable function

--- DMXDyn/module.php
+++ DMXDyn/module.php
@@ -28,6 +28,23 @@
             parent::ApplyChanges();
         }
  
+        // Erstellt eine Instanz Variable
+        protected function CreateVariable($type, $name, $ident, $profile, $actionID, $parent = "thisInstance", $position = 0, $initVal = 0){
+            if($parent == "thisInstance")
+                $parent = $this->InstanceID;
+            $vid = IPS_CreateVariable($type);
+            IPS_SetName($vid,$name);
+            IPS_SetParent($vid,$parent);
+            IPS_SetIdent($vid,$ident);
+            IPS_SetPosition($vid,$position);
+            IPS_SetVariableCustomProfile($vid,$profile);
+            if($actionID > 9999)
+                IPS_SetVariableCustomAction($vid,$actionID);
+            SetValue($vid,$initVal);
+            
+            return $vid;
+        }
+
         // Own Function
         public function ownFirstFunction() {
            
