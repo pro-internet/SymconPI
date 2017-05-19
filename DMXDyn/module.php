c9bac6ff30e90ccd2b4d2eba84e7d982c7acfcd0
Alex Wellnitz <wellnitz.alex@gmail.com>
2017-05-19 11:40:02 +0200

    Create Instance Vars

--- DMXDyn/module.php
+++ DMXDyn/module.php
@@ -13,7 +13,10 @@
             parent::Create();
 
 
-            $rWert     = CreateVariable("rWert",      3 /*String*/,  $categoryId_DataValues,  140, '~String', $scriptId_Refresh, '');
+            // Create Instance Vars
+            $VarID_RWert = IPS_CreateVariable(1);
+            IPS_SetName($VarID_RWertr, "R Standart Wert");
+            IPS_SetParent($VarID_RWertr, 44440);
 
           
           /*
@@ -28,22 +31,7 @@
             parent::ApplyChanges();
         }
  
-        // Erstellt eine Instanz Variable
-        protected function CreateVariable($type, $name, $ident, $profile, $actionID, $parent = "thisInstance", $position = 0, $initVal = 0){
-            if($parent == "thisInstance")
-                $parent = $this->InstanceID;
-            $vid = IPS_CreateVariable($type);
-            IPS_SetName($vid,$name);
-            IPS_SetParent($vid,$parent);
-            IPS_SetIdent($vid,$ident);
-            IPS_SetPosition($vid,$position);
-            IPS_SetVariableCustomProfile($vid,$profile);
-            if($actionID > 9999)
-                IPS_SetVariableCustomAction($vid,$actionID);
-            SetValue($vid,$initVal);
-            
-            return $vid;
-        }
+
 
         // Own Function
         public function ownFirstFunction() {
