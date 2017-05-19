28ab9a59224981d81a25d20b78f1f88b1c5ea676
Alex Wellnitz <wellnitz.alex@gmail.com>
2017-05-19 11:55:44 +0200

    Adding all Instance Vars (RGBW)

--- DMXDyn/module.php
+++ DMXDyn/module.php
@@ -15,10 +15,22 @@
 
 			$parent = $this->InstanceID;
 
-            // Create Instance Vars
+            // Create Instance Vars (RGBW)
             $VarID_RWert = IPS_CreateVariable(1);
             IPS_SetName($VarID_RWert, "R Standart Wert");
             IPS_SetParent($VarID_RWert, $parent);
+
+            $VarID_GWert = IPS_CreateVariable(1);
+            IPS_SetName($VarID_GWert, "G Standart Wert");
+            IPS_SetParent($VarID_GWert, $parent);
+
+            $VarID_BWert = IPS_CreateVariable(1);
+            IPS_SetName($VarID_BWert, "B Standart Wert");
+            IPS_SetParent($VarID_BWert, $parent);
+
+            $VarID_WWert = IPS_CreateVariable(1);
+            IPS_SetName($VarID_WWert, "W Standart Wert");
+            IPS_SetParent($VarID_WWert, $parent);
           
           /*
             $this->RegisterPropertyString("woeid", "701780");
