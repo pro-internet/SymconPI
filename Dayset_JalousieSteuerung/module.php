a6b68145526dd18ea4bf0b32948cab14f53b0f75
DavidCks <david.christ99@live.de>
2017-05-22 19:32:06 +0200

    Added DaysetJal

---
+++ Dayset_JalousieSteuerung/module.php
@@ -0,0 +1,26 @@
+<?
+class TestModule extends IPSModule {
+
+	public function Create() {
+		//Never delete this line!
+		parent::Create();
+
+	}
+
+	public function Destroy() {
+		//Never delete this line!
+		parent::Destroy();
+		
+	}
+
+	public function ApplyChanges() {
+		//Never delete this line!
+		parent::ApplyChanges();
+		
+	}
+	
+	////////////////////
+	//public functions//
+	////////////////////
+}
+?>
